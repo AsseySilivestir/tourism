82abb7390d68b6126c3e1fcc986ec28ffda619be
minjaezekiel <minjaezekiel@gmail.com>
2025-11-11 16:42:28 +0300

    conf client side rendering - functional

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,20 @@ const port = process.env.PORT||3000
 const frontendPath = path.join(__dirname, './../tce_frontend/dist')
 
 //middleware
-app.use(express.json())
+app.use(express.json()) 
 //serving static files from the dist dir...(frontend build)
 app.use(express.static(frontendPath,{index:false}))
-
-
+/*
+* Even this opt works...does same thing as app.use(express.static(frontendPath,{index:false}))
+app.use('/assets', express.static(path.join(frontendPath, 'assets')));
+*/
 
 //connecting to database...
 connectDB()
-app.get("/",(req,res)=>{res.send("<h1>Hello world</h1>")})
+/*app.get("/",(req,res)=>{
+  res.send("<h1>Hello world</h1>")
+  console.log('working')
+})*/
 
 /*
  *splat matches any path without the root path. If you need to match the root 
@@ -25,7 +30,7 @@ app.get("/",(req,res)=>{res.send("<h1>Hello world</h1>")})
 app.get('/{*splat}', (req, res) => {
   console.log("Catch-all route hit. Sending file:", path.join(frontendPath, "index.html"));
   res.sendFile(path.join(frontendPath, "index.html"));
-});
+}); 
 
 //to run the server, type in command prompt npm run devStart
 //note that you must be in the same directory as the backend/server.js
